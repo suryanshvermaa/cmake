318cfb43519b8df1b71bc9584d9a0b26732d8edf
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-12 20:38:06 +0530

    multifile app

---
+++ MultiFileProject/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"dog.h"
+#include"operations.h"
+using namespace std;
+int main(){
+    Dog dog1("Fliftzy");
+    dog1.print_info();
+    cout<<add(4,6)<<endl;
+    return 0;
+}
